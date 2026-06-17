b3476ff86dbc6b0df89a94958087f494fce4bffb
PJSans <PJSans@Dontcontact.me>
2026-06-16 22:43:56 -0600

    IT IS MOSTLY WORKINGGG

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pygame
+import pygame,sys
 from constants import SCREEN_SIZE
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,23 +11,36 @@ def main():
     pygame.display.set_caption("Asteroids")
     clock = pygame.time.Clock()
     dt = 0
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    AsteroidField()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 2)
     print(SCREEN_SIZE)
     print(f"Screen width: {SCREEN_SIZE[0]}, Screen height: {SCREEN_SIZE[1]}")
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    while True:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                break
-            pass
+                running = False
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
         pygame.display.flip()
         log_state()
         dt = clock.tick(60) /1000
-        player.update(dt)
-        
-
+        for sprite in updatable:
+            sprite.update(dt)
+        for i in range(len(asteroids)):
+            asteroid = asteroids.sprites()[i]
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                sys.exit()
 
 if __name__ == "__main__":
     main()
